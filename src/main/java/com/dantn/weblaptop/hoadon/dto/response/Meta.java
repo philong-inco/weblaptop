e3e542521a407274bcaeab715f7858ee8534fffe
manh_03 <nguyenmanh19924@gmail.com>
2024-07-03 18:54:06 +0700

    Lich Su Hoa Don v1

---
+++ src/main/java/com/dantn/weblaptop/hoadon/dto/response/Meta.java
@@ -0,0 +1,18 @@
+package com.dantn.weblaptop.hoadon.dto.response;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.FieldDefaults;
+
+@Getter
+@Setter
+@Builder
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class Meta {
+    Integer page;
+    Integer pageSize;
+    Integer pages;
+    Long total;
+}
