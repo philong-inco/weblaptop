e8d686ba0dae3f78800f521c4f022c16b3a61e5f
manh_03 <nguyenmanh19924@gmail.com>
2024-07-07 00:16:37 +0700

    delete package hoadon

--- src/main/java/com/dantn/weblaptop/hoadon/fake/NhanVienRepositoryFAKE.java
+++
@@ -1,9 +0,0 @@
-package com.dantn.weblaptop.hoadon.fake;
-
-import com.dantn.weblaptop.entity.nhanvien.NhanVien;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface NhanVienRepositoryFAKE extends JpaRepository<NhanVien, Long> {
-}
