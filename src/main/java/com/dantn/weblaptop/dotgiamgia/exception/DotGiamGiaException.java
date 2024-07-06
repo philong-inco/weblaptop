a9cd400a6d14e2cc10febb3f67e3f43994d5c4e1
Vũ Hoàng Huy <huyvh04@gmail.com>
2024-07-06 10:45:20 +0700

    Kiem Tra Bi Loi O Phan San Pham Chi Tiet

--- src/main/java/com/dantn/weblaptop/dotgiamgia/exception/DotGiamGiaException.java
+++ src/main/java/com/dantn/weblaptop/dotgiamgia/exception/DotGiamGiaException.java
@@ -1,11 +1,13 @@
 package com.dantn.weblaptop.dotgiamgia.exception;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
-public class GlobalException {
+@Qualifier("dotGiamGiaException")
+public class DotGiamGiaException {
     @ExceptionHandler(value = RuntimeException.class)
     ResponseEntity<String> runtimeExceptionHandler(RuntimeException e) {
         return ResponseEntity.badRequest().body(e.getMessage());
