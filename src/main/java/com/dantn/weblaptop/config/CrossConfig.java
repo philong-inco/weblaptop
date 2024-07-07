7c9f1daa00f67edf0380596149660940bc119cd6
manh_03 <nguyenmanh19924@gmail.com>
2024-07-07 18:10:46 +0700

    CONFIX CROSS

---
+++ src/main/java/com/dantn/weblaptop/config/CrossConfig.java
@@ -0,0 +1,23 @@
+package com.dantn.weblaptop.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+
+@Configuration
+public class CrossConfig {
+
+    @Bean
+    public CorsFilter corsFilter (){
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.addAllowedOrigin("http://localhost:5173");
+        corsConfiguration.addAllowedMethod("*");
+        corsConfiguration.addAllowedHeader("*");
+        UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
+        urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
+        return new CorsFilter(urlBasedCorsConfigurationSource);
+    }
+}
--- src/main/java/com/dantn/weblaptop/controller/PhieuGiamGiaController.java
+++ src/main/java/com/dantn/weblaptop/controller/PhieuGiamGiaController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/coupons")
-@CrossOrigin("*")
 public class PhieuGiamGiaController {
     @Autowired
     private PhieuGiamGiaService phieuGiamGiaService;
