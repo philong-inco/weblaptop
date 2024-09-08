c3eea6d545f87fa05b381258c08e79de6d94016c
DESKTOP-54BBH23\vutu8 <ewfngoctu@gmail.com>
2024-09-08 23:47:59 +0700

    Update Config

--- src/main/java/com/dantn/weblaptop/repository/SerialNumberRepository.java
+++ src/main/java/com/dantn/weblaptop/repository/SerialNumberRepository.java
@@ -1,22 +1,20 @@
 package com.dantn.weblaptop.repository;
 
 import com.dantn.weblaptop.entity.sanpham.SerialNumber;
-<<<<<<< HEAD
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-=======
-import org.springframework.data.jpa.repository.JpaRepository;
->>>>>>> manhntph37150
+
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface SerialNumberRepository extends JpaRepository<SerialNumber, Long> {
-<<<<<<< HEAD
+
 
     @Query("SELECT s FROM SerialNumber s")
     List<SerialNumber> getAllList();
@@ -28,11 +26,7 @@ public interface SerialNumberRepository extends JpaRepository<SerialNumber, Long
     List<SerialNumber> existByMaForAdd(@Param("ma") String ma);
     @Query("SELECT s FROM SerialNumber s WHERE lower(s.ma) = :ma AND s.id <> :id")
     List<SerialNumber> existByMaForUpdate(@Param("ma") String ma, @Param("id") Long id);
-
     SerialNumber findByMa(String ma);
-=======
     List<SerialNumber> findBySanPhamChiTietIdAndTrangThai(Long productDetailId, Integer status);
-
-    List<SerialNumber> findAllBySanPhamChiTietId(Long productDetailId);
->>>>>>> manhntph37150
+    Page<SerialNumber> findBySanPhamChiTietIdAndTrangThai(Long productDetailId, Integer status, Pageable pageable);
 }
