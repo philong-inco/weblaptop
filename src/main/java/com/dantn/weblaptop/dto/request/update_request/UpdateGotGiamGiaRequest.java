cabf0010367aad849094466d9849f03cf072b944
DESKTOP-SNQDB9O\Dell <yet26379@gmail.com>
2024-09-22 20:56:02 +0700

    Update backend DGG by ThanhThao

--- src/main/java/com/dantn/weblaptop/dto/request/update_request/UpdateGotGiamGiaRequest.java
+++ src/main/java/com/dantn/weblaptop/dto/request/update_request/UpdateGotGiamGiaRequest.java
@@ -1,7 +1,10 @@
 package com.dantn.weblaptop.dto.request.update_request;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import jakarta.validation.constraints.*;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
 
@@ -16,24 +19,21 @@ import java.util.List;
 @ToString
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class UpdateGotGiamGiaRequest {
-    String ma;
     @NotBlank(message = "Tên Đợt Giảm Giá Trống !")
     @Size(min = 1, max = 255, message = "Tên Vượt Quá 255 ký tự !")
     String ten;
     Integer trangThai;
-    @NotBlank(message = "Mô Tả Đợt Giảm Giá Trống !")
-    @Size(min = 1, max = 255, message = "Mô Tả Vượt Quá 255 ký tự !")
     String moTa;
-    @NotBlank(message = "Loại Chiết Khấu không được để trống!")
     Integer loaiChietKhau;
-    @NotNull(message = "=START_DATE_NOT_NULL")
+    @NotNull(message = "START_DATE_NOT_NULL")
     @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
     LocalDateTime thoiGianBatDau;
     @NotNull(message = "END_DATE_NOT_NULL")
     @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
     LocalDateTime thoiGianKetThuc;
-    @NotNull(message = "COUPONS_DISCOUNT_VALUE_NOT_NULL")
-    @Min(value = 1, message = "COUPONS_MIN_DISCOUNT_VALUE")
+    @DecimalMin(value = "1.00", message = "COUPONS_MIN_DISCOUNT_VALUE")
     BigDecimal giaTriGiam;
+
     List<Long> listSanPhamChiTiet;
 }
+
