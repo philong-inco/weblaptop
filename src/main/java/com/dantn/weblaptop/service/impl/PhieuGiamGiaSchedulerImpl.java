9921b3e6a27d6069fb33949bdcce9d6223d5883a
DESKTOP-54BBH23\vutu8 <ewfngoctu@gmail.com>
2024-10-14 11:12:53 +0700

    Update schedule

--- src/main/java/com/dantn/weblaptop/service/impl/PhieuGiamGiaSchedulerImpl.java
+++ src/main/java/com/dantn/weblaptop/service/impl/PhieuGiamGiaSchedulerImpl.java
@@ -16,27 +16,31 @@ public class PhieuGiamGiaSchedulerImpl implements PhieuGiamGiaScheduler {
     private PhieuGiamGiaRepo phieuGiamGiaRepo;
 
     @Override
-    @Scheduled(fixedRate = 10000) // Chạy mỗi 60 giây
+    @Scheduled(fixedRate = 60000) // Chạy mỗi 60 giây
     public void updateDiscountsStatus() {
         LocalDateTime now = LocalDateTime.now();
         List<PhieuGiamGia> allPhieuGiamGia = phieuGiamGiaRepo.findAll();
 
         for (PhieuGiamGia phieuGiamGia : allPhieuGiamGia) {
             if (phieuGiamGia.getTrangThai() == 4) {
-                continue; // Bỏ qua phiếu giảm giá với trạng thái 4
+                continue; // Bỏ qua phiếu giảm giá với trạng thái 4 (tạm dừng)
             }
-            if(phieuGiamGia.getSoLuong() == 0){
+
+            if (phieuGiamGia.getSoLuong() == 0) {
                 phieuGiamGia.setTrangThai(2);
-            }else if (phieuGiamGia.getTrangThai() != 3) { // Chỉ cập nhật nếu phiếu giảm giá không bị hủy
-                if (phieuGiamGia.getNgayHetHan().isBefore(now)) {
-                    phieuGiamGia.setTrangThai(2); // Cập nhật trạng thái thành "Hết hạn"
-                } else if (phieuGiamGia.getNgayBatDau().isAfter(now)) {
-                    phieuGiamGia.setTrangThai(0); // Cập nhật trạng thái thành "Chưa áp dụng"
+            } else if (phieuGiamGia.getTrangThai() != 3) {
+
+                if (phieuGiamGia.getNgayBatDau().isAfter(now)) {
+                    phieuGiamGia.setTrangThai(0);
                 } else if (phieuGiamGia.getNgayBatDau().isBefore(now) && phieuGiamGia.getNgayHetHan().isAfter(now)) {
-                    phieuGiamGia.setTrangThai(1); // Cập nhật trạng thái thành "Đang áp dụng"
+                    phieuGiamGia.setTrangThai(1);
+                } else if (phieuGiamGia.getNgayHetHan().isBefore(now) || phieuGiamGia.getNgayHetHan().isEqual(now)) {
+                    phieuGiamGia.setTrangThai(2);
                 }
             }
+
             phieuGiamGiaRepo.save(phieuGiamGia);
         }
     }
+
 }
