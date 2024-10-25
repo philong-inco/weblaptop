82a87e9e1ac185678adf27a6be05c08ea9e879cc
manh_03 <nguyenmanh19924@gmail.com>
2024-10-25 23:04:55 +0700

    validate addToCartRequest

--- src/main/java/com/dantn/weblaptop/dto/request/create_request/AddToGioHangRequest.java
+++ src/main/java/com/dantn/weblaptop/dto/request/create_request/AddToGioHangRequest.java
@@ -1,5 +1,7 @@
 package com.dantn.weblaptop.dto.request.create_request;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.Setter;
@@ -13,7 +15,10 @@ import java.math.BigDecimal;
 public class AddToGioHangRequest {
     String sessionId;
     Long idKhachHang;
+    @NotNull(message = "PRODUCT_DETAIL_IS_NULL")
     Long idSPCT;
+    @NotNull(message = "QUANTITY_PRODUCT_DETAIL_IN_CART_NOT_NULL")
+    @Min(value = 1, message = "MINIMUM_NUMBER_OF_PRODUCTS_IS_1")
     Integer soLuong;
 //    BigDecimal gia;
 }
