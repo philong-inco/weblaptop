acc16fb89e69477f8643e1398d786aa2a661b1bf
philong-inco <philong.inco@gmail.com>
2024-11-01 12:32:33 +0700

    bổ sung thông tin trả ra khi đăng nhập

--- src/main/java/com/dantn/weblaptop/config/dto/AuthResponse.java
+++ src/main/java/com/dantn/weblaptop/config/dto/AuthResponse.java
@@ -2,9 +2,18 @@ package com.dantn.weblaptop.config.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
+@Setter
+@NoArgsConstructor
 @AllArgsConstructor
 public class AuthResponse {
     private String jwt;
+    private String role;
+    private Long id;
+    private String ten;
+    private String email;
+    private String sdt;
 }
